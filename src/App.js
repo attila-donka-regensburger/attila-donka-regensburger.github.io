a79a637a2466292278f41b5e3e5b387e159ae762
attila-donka-regensburger <dasap.dasco@gmail.com>
2023-10-01 16:12:20 +0200

    try

--- src/App.js
+++ src/App.js
@@ -7,7 +7,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Portfolio: <code></code> Attila Donka Regensburger
+        </p>
+        <p>
+          Here comes my portfolio or aganda, please wait because this only the test page.
         </p>
         <a
           className="App-link"
